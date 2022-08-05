5387cf8497b3585f73498a1b5649e8693212464b
GedeD <dyavas21@gmail.com>
2022-08-05 22:33:09 +0700

    routing splash - main

--- app/lib/ui/pages/splash_page.dart
+++ app/lib/ui/pages/splash_page.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:app/shared/theme.dart';
 import 'package:app/ui/pages/get_started_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
@@ -15,7 +16,14 @@ class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
     Timer(Duration(seconds: 3), (() {
-      Navigator.pushNamed(context, '/get-started');
+      User? user = FirebaseAuth.instance.currentUser;
+
+      if (user == null) {
+        Navigator.pushNamedAndRemoveUntil(
+            context, '/get-started', (route) => false);
+      } else {
+        Navigator.pushNamedAndRemoveUntil(context, '/main', (route) => false);
+      }
     }));
     super.initState();
   }
